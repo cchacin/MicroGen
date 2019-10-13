8966a3ce77f69a1b40f3d9c60253f419cbcd02b9
Carlos Chacin <cchacin@gmail.com>
2019-10-12 21:36:43 -0700

    Clean up

--- microgen-codegen/src/test/resources/fixtures/example/src/main/java/org/openapitools/server/model/BaseCard.java
+++ microgen-codegen/src/test/resources/fixtures/example/src/main/java/org/openapitools/server/model/BaseCard.java
@@ -1,15 +1,6 @@
 package org.openapitools.server.model;
 
 /** This is a base card object which uses a &#39;cardType&#39; discriminator. */
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "cardType",
-        visible = true)
-@JsonSubTypes({
-                      @JsonSubTypes.Type(value = PlaceCard.class, name = "PlaceCard"),
-                      @JsonSubTypes.Type(value = PersonCard.class, name = "PersonCard"),
-              })
 @org.immutables.value.Value.Immutable
 @org.immutables.value.Value.Style(
         defaultAsDefault = true,
