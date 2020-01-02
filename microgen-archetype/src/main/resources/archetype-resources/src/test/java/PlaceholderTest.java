d797463e27af32396bc94466d3ef4cd56cc682cc
Carlos Chacin <cchacin@gmail.com>
2020-01-02 03:42:25 -0800

    feat: Enable Junit 5 in the archetype

---
+++ microgen-archetype/src/main/resources/archetype-resources/src/test/java/PlaceholderTest.java
@@ -0,0 +1,17 @@
+package ${package};
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+
+class PlaceholderTest implements WithAssertions {
+
+    @Test
+    void test() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+        assertThat(1).isEqualTo(2 - 1);
+    }
+}
