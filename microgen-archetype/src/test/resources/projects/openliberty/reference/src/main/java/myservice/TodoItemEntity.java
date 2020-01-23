5fb1ca50aca9c7c06b366f7c984247a6170dd2db
Carlos Chacin <cchacin@gmail.com>
2020-01-23 02:31:41 -0800

    feat: Add JPA entity and deltaspike configs (#232)

---
+++ microgen-archetype/src/test/resources/projects/openliberty/reference/src/main/java/myservice/TodoItemEntity.java
@@ -0,0 +1,77 @@
+package myservice;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class TodoItemEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "created_on",
+            columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private LocalDateTime createdOn;
+
+    @Column(name = "done_at",
+            columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private LocalDateTime doneAt;
+
+    public TodoItemEntity() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getDoneAt() {
+        return doneAt;
+    }
+
+    public void setDoneAt(LocalDateTime updatedAt) {
+        this.doneAt = updatedAt;
+    }
+}
